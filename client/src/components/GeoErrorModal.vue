94dcfec44679b530995b46fb0bfa123a591bc40e
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-07 23:18:40 -0300

    Update Geocoding

---
+++ client/src/components/GeoErrorModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-screen absolute w-full z-10 flex justify-center items-start pt-[125px] bg-black/50">
+    <div class="flex flex-col bg-white w-[80%] sm:w-[450px] px-6 py-4 rounded-md">
+      <h1 class="text-lg mb-1"> Error: </h1>
+      <p class="text-sm mb-4">
+        To take advantage of this application features, please ensure the location services are enabled
+      </p>
+      <button class="self-start py-2 px-4 bg-red-500 text-white rounded-md text-sm">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="h-screen relative">
+        <GeoErrorModal/>
         <div id="map" class="h-full z-[1]"></div>
     </div>
 </template>
@@ -7,17 +8,15 @@
 <script>
 import leaflet from 'leaflet';
 import { onMounted, ref } from "vue";
+import GeoErrorModal from '@/components/GeoErrorModal.vue';
 
 export default {
     name: 'HomeView',
-    components: {},
+    components: { GeoErrorModal },
     setup() {
         let map;
         onMounted(() => {
-            // init map
             map = leaflet.map('map').setView([51.505, -0.09], 13);
-
-            // add tile layers
             leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`, {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
@@ -26,26 +25,64 @@ export default {
                 zoomOffset: -1,
                 accessToken: process.env.VUE_APP_API_KEY
             }).addTo(map);
-            // get users location
             getGeolocation();
         });
 
-        // const coords = ref(null);
-        const fetchCoords = ref(null);
-        // const geoMarker = ref(null);
+        const coords = ref(null);
+        const fetchCoordinates = ref(null);
+        const geoMarker = ref(null);
+        const geoError = ref(null);
+        const geoErrorMessage = ref(null);
 
         const getGeolocation = () => {
-            fetchCoords.value = true;
-            navigator.geolocation.getCurrentPosition(setCoords, getLocationError);
+            if (sessionStorage.getItem('coords')) {
+                coords.value = JSON.parse(sessionStorage.getItem('coords'));
+                plotGeolocation(coords.value);
+                return;
+            }
+
+            fetchCoordinates.value = true;
+            navigator.geolocation.getCurrentPosition(setCoordinates, getLocationError);
         };
 
-        const setCoords = (position) => {
-            console.log(position)
+        const setCoordinates = (position) => {
+            fetchCoordinates.value = null;
+
+            const setSessionCoordinates = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+
+            sessionStorage.setItem('coords', JSON.stringify(setSessionCoordinates));
+            coords.value = setSessionCoordinates;
+            plotGeolocation(coords.value);
         };
+
+        const plotGeolocation = (coords) => {
+            const customMarker = leaflet.icon({
+                iconUrl: require('../assets/map-marker-red.svg'),
+                iconSize: [35, 35]
+            });
+
+            geoMarker.value = leaflet
+                .marker([coords.lat, coords.lng], { icon: customMarker })
+                .addTo(map);
+
+            map.setView([coords.lat, coords.lng], 10);
+        }
 
         const getLocationError = (error) => {
-            console.log(error)
+            fetchCoordinates.value = null;
+            geoError.value = true;
+            geoErrorMessage.value = error.message;
         };
+
+        const closeGeolocationError = () => {
+            geoError.value = null;
+            geoErrorMessage.value = null;
+        };
+
+        return { coords, geoMarker, closeGeolocationError }
     }
 };
 </script>
