7df993e82fecd6cb91cb6ed126977de7c208fc96
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-08 01:04:59 -0300

    Update home view

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,90 +1,159 @@
 <template>
-	<div class="h-screen relative">
-		<GeoErrorModal @closeGeoError="closeGeolocationError" v-if="geoError" :geoErrorMEssage="geoErrorMessage"/>
-		<div id="map" class="h-full z-[1]"></div>
-
-    <MapFeatures :coords="coords" :fetchCoordinates="fetchCoordinates"/>
-	</div>
+  <div class="h-screen relative">
+    <GeoErrorModal
+      v-if="geoError"
+      :geoErrorMsg="geoErrorMsg"
+      @closeGeoError="closeGeoError"
+    />
+    <MapFeatures
+      :fetchCoords="fetchCoords"
+      :coords="coords"
+      @toggleSearchResults="toggleSearchResults"
+      @getGeolocation="getGeolocation"
+      @plotResult="plotResult"
+      @removeResult="removeResult"
+      :searchResults="searchResults"
+      class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2]"
+    />
+    <div id="mapid" class="h-full z-[1]"></div>
+  </div>
 </template>
 
 <script>
-import leaflet from 'leaflet';
+import leaflet from "leaflet";
 import { onMounted, ref } from "vue";
-import GeoErrorModal from '@/components/GeoErrorModal.vue';
-import MapFeatures from '@/components/MapFeatures.vue';
+import GeoErrorModal from "../components/GeoErrorModal.vue";
+import MapFeatures from "../components/MapFeatures.vue";
 
 export default {
-	name: 'HomeView',
-	components: { GeoErrorModal, MapFeatures },
-	setup() {
-		let map;
-		onMounted(() => {
-			map = leaflet.map('map').setView([51.505, -0.09], 13);
-			leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`, {
-				attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-				maxZoom: 18,
-				id: 'mapbox/streets-v11',
-				tileSize: 512,
-				zoomOffset: -1,
-				accessToken: process.env.VUE_APP_API_KEY
-			}).addTo(map);
-			getGeolocation();
-		});
+  name: "HomeView",
+  components: { GeoErrorModal, MapFeatures },
+  setup() {
+    let map;
+    onMounted(() => {
+      map = leaflet
+        .map("mapid", {
+          zoomControl: false,
+        })
+        .setView([28.538336, -81.379234], 10);
 
-		const coords = ref(null);
-		const fetchCoordinates = ref(null);
-		const geoMarker = ref(null);
-		const geoError = ref(null);
-		const geoErrorMessage = ref(null);
+      leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`, {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: process.env.VUE_APP_API_KEY,
+      }).addTo(map);
 
-		const getGeolocation = () => {
-			if (sessionStorage.getItem('coords')) {
-					coords.value = JSON.parse(sessionStorage.getItem('coords'));
-					plotGeolocation(coords.value);
-					return;
-			}
+      map.on("moveend", () => {
+        closeSearchResults();
+      });
 
-			fetchCoordinates.value = true;
-			navigator.geolocation.getCurrentPosition(setCoordinates, getLocationError);
-		};
+      getGeolocation();
+    });
 
-		const setCoordinates = (position) => {
-			fetchCoordinates.value = null;
+    const coords = ref(null);
+    const fetchCoords = ref(null);
+    const geoMarker = ref(null);
+    const geoError = ref(null);
+    const geoErrorMsg = ref(null);
 
-			const setSessionCoordinates = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-			}
+    const getGeolocation = () => {
+      if (!coords.value) {
+        if (sessionStorage.getItem("coords")) {
+          coords.value = JSON.parse(sessionStorage.getItem("coords"));
+          plotGeoLocation(coords.value);
+          return;
+        }
 
-			sessionStorage.setItem('coords', JSON.stringify(setSessionCoordinates));
-			coords.value = setSessionCoordinates;
-			plotGeolocation(coords.value);
-		};
+        fetchCoords.value = true;
+        navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+        return;
+      }
 
-		const plotGeolocation = (coords) => {
-			const customMarker = leaflet.icon({
-				iconUrl: require('../assets/map-marker-red.svg'),
-				iconSize: [35, 35]
-			});
+      coords.value = null;
+      sessionStorage.removeItem("coords");
+      map.removeLayer(geoMarker.value);
+    };
 
-			geoMarker.value = leaflet
-			.marker([coords.lat, coords.lng], { icon: customMarker })
-			.addTo(map);
-			map.setView([coords.lat, coords.lng], 10);
-		}
+    const setCoords = (pos) => {
+      fetchCoords.value = null;
 
-		const getLocationError = (error) => {
-			fetchCoordinates.value = null;
-			geoError.value = true;
-			geoErrorMessage.value = error.message;
-		};
+      const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+      sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
 
-		const closeGeolocationError = () => {
-			geoError.value = null;
-			geoErrorMessage.value = null;
-		};
+      coords.value = setSessionCoords;
 
-		return { coords, geoMarker, closeGeolocationError, geoError, geoErrorMessage, fetchCoordinates }
-	}
+      plotGeoLocation(coords.value);
+    };
+
+    const getLocError = (error) => {
+      fetchCoords.value = null;
+      geoError.value = true;
+      geoErrorMsg.value = error.message;
+    };
+
+    const plotGeoLocation = (coords) => {
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/map-marker-red.svg"),
+        iconSize: [35, 35],
+      });
+
+      geoMarker.value = leaflet
+        .marker([coords.lat, coords.lng], { icon: customMarker })
+        .addTo(map);
+
+      map.setView([coords.lat, coords.lng], 10);
+    };
+
+    const resultMarker = ref(null);
+    const plotResult = (coords) => {
+      if (resultMarker.value) {
+        map.removeLayer(resultMarker.value);
+      }
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/map-marker-blue.svg"),
+      });
+      resultMarker.value = leaflet
+        .marker([coords.coordinates[1], coords.coordinates[0]], { icon: customMarker })
+        .addTo(map);
+      map.setView([coords.coordinates[1], coords.coordinates[0]], 13);
+    };
+
+    const removeResult = () => {
+      map.removeLayer(resultMarker.value);
+    };
+
+    const closeGeoError = () => {
+      geoErrorMsg.value = null;
+      geoError.value = null;
+    };
+
+    const searchResults = ref(null);
+    const toggleSearchResults = () => {
+      searchResults.value = !searchResults.value;
+    };
+    const closeSearchResults = () => {
+      searchResults.value = null;
+    };
+
+    return {
+      geoError,
+      closeGeoError,
+      geoErrorMsg,
+      fetchCoords,
+      coords,
+      getGeolocation,
+      plotResult,
+      searchResults,
+      toggleSearchResults,
+      closeSearchResults,
+      removeResult,
+    };
+  },
 };
 </script>
