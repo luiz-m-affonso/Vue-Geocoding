66d160f285e472b3fc97358f6684a83a678d129f
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-06-08 01:04:28 -0300

    Update geo error modal

--- client/src/components/GeoErrorModal.vue
+++ client/src/components/GeoErrorModal.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="h-screen absolute w-full z-10 flex justify-center items-start pt-[125px] bg-black/50">
+  <div
+    class="h-screen absolute w-full z-10 flex justify-center items-start pt-[125px] bg-black/50"
+  >
     <div class="flex flex-col bg-white w-[80%] sm:w-[450px] px-6 py-4 rounded-md">
-      <h1 class="text-lg mb-1">Error: {{ geoErrorMessage }} </h1>
+      <h1 class="text-lg mb-1">Error: {{ this.geoErrorMsg }}</h1>
       <p class="text-sm mb-4">
-        To take advantage of this application features, please ensure the location services are enabled
+        To take advantage of this applications features, please ensure location services
+        are enabled
       </p>
-      <button @click="$emit('closeGeoError')" class="self-start py-2 px-4 bg-red-500 text-white rounded-md text-sm">
+      <button
+        @click="$emit('closeGeoError')"
+        class="self-start py-2 px-4 bg-red-500 text-white rounded-md text-sm"
+      >
         Close
       </button>
     </div>
@@ -14,10 +20,6 @@
 
 <script>
 export default {
-  pros: ['geoErrorMessage'],
+  props: ["geoErrorMsg"],
 };
 </script>
-
-<style>
-
-</style>
